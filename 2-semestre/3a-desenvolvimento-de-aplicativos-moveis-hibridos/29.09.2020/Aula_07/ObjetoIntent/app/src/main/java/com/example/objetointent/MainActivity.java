a0a96f72461e079b473a16c1ce1fc06cdf6f06e3
igorlimamendes <igorlimamendes@protonmail.com>
2020-09-29 21:59:21 -0300

    Add files

---
+++ 2-semestre/3a-desenvolvimento-de-aplicativos-moveis-hibridos/29.09.2020/Aula_07/ObjetoIntent/app/src/main/java/com/example/objetointent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.objetointent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText nome, sobrenome;
+    Button btProxima;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nome = findViewById(R.id.editTextNome);
+        sobrenome = findViewById(R.id.editTextSobrenome);
+        btProxima = findViewById(R.id.buttonProxima);
+
+        btProxima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), Resultado.class);
+
+                // Vamos criar um objeto Pessoa
+                Pessoa p = new Pessoa();
+                p.setNome(nome.getText().toString());
+                p.setSobrenome(sobrenome.getText().toString());
+
+                // Atribuindo o objeto à Intent
+                it.putExtra("pessoa", p);
+
+                // chama a outra activity
+                startActivity(it);
+            }
+        });
+
+    }
+}
