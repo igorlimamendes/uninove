fb3e106e229047eadc1c457c61b5fe8679d4ea18
igorlimamendes <igorlimamendes@protonmail.com>
2020-10-15 10:41:59 -0300

    Add URI 1096 C solution

---
+++ 2-semestre/uri-online-judge/c/1096.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+    int i;
+    
+    for (i = 1; i <= 9; i += 2) {
+        int j = 7;
+        printf("I=%d J=%d\n", i, j);
+        j--;
+        printf("I=%d J=%d\n", i, j);
+        j--;
+        printf("I=%d J=%d\n", i, j);
+    }
+    
+    return 0;
+}
