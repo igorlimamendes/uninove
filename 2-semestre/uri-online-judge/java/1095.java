cda03a7e2044933dcdf566ddde57e31654de5100
igorlimamendes <igorlimamendes@protonmail.com>
2020-10-15 10:40:37 -0300

    Add URI 1095 Java solution

---
+++ 2-semestre/uri-online-judge/java/1095.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int i = 1;
+
+        for (int j = 60; j >= 0; j -= 5) {
+            System.out.printf("I=%d J=%d\n", i, j);
+
+            i += 3;
+        }
+    }
+}
