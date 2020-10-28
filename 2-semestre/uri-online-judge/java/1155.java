4508cf749ecd4f5c148fb42859c5672e7951ad6d
igorlimamendes <igorlimamendes@protonmail.com>
2020-10-28 10:15:51 -0300

    Add URI 1155 Java solution

---
+++ 2-semestre/uri-online-judge/java/1155.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        double s = 1;
+
+        for (int i = 2; i <= 100; i++) {
+            s += (1.0 / i);
+        }
+
+        System.out.printf("%.2f\n", s);
+    }
+}
