8388015ec4037eb8f49d43c21d51fa70b47e9fbb
igorlimamendes <igorlimamendes@protonmail.com>
2020-11-14 09:53:07 -0300

    Friday class files

---
+++ 2-semestre/6a-programacao-orientada-a-objetos/13.11.2020/Prj_CadastroCliente/src/prj_cadastrocliente/Prj_CadastroCliente.java
@@ -0,0 +1,10 @@
+package prj_cadastrocliente;
+
+public class Prj_CadastroCliente {
+
+    public static void main(String[] args) {
+        frmClientes X =new frmClientes();
+        X.setVisible(true);
+    }
+    
+}
